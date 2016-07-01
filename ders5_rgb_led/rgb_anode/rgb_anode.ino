b1d3b0f0d434736b7fe1cf88b814a23fe32430f2
Yusuf Gündoğdu <yusuf@bomec.com.tr>
2016-07-01 14:50:04 +0300

    Add files via upload

---
+++ ders5_rgb_led/rgb_anode/rgb_anode.ino
@@ -0,0 +1,40 @@
+int kirmiziPin = 3;
+int yesilPin = 5;
+int maviPin = 6;
+
+
+void setup()
+{
+  pinMode(kirmiziPin, OUTPUT);
+  pinMode(yesilPin, OUTPUT);
+  pinMode(maviPin, OUTPUT);  
+}
+
+void loop()
+{
+  renkAyarla(255, 0, 0);  // kirmizi
+  delay(1500);
+  renkAyarla(0, 255, 0);  // yesil
+  delay(1500);
+  renkAyarla(0, 0, 255);  // mavi
+  delay(1500);
+  renkAyarla(255, 255, 0);  // sari
+  delay(1500);  
+  renkAyarla(80, 0, 80);  // mor
+  delay(1500);
+  renkAyarla(0, 255, 255);  // acik mavi
+  delay(1500);
+  renkAyarla(255, 255, 255); // beyaz
+  delay(1500);
+}
+
+void renkAyarla(int kirmizi, int yesil, int mavi)
+{
+  kirmizi = 255 - kirmizi;
+  yesil = 255 - yesil;
+  mavi = 255 - mavi;
+  analogWrite(kirmiziPin, kirmizi);
+  analogWrite(yesilPin, yesil);
+  analogWrite(maviPin, mavi);  
+}
+
