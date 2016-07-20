f9d92425dd36b41b2ccac242e3d23838782a1b5b
Robotistan <kerem@bomec.com.tr>
2016-07-20 14:46:07 +0300

    Revert "düzenleme"
    This reverts commit b3af19f6b9e3e01ba04ba98f3ea1d5f5d7eeed28.

---
+++ ders3_analog_read/analog_read/analog_read.ino
@@ -0,0 +1,32 @@
+int potPin = 0;
+int latchPin = 5;
+int clockPin = 6;
+int dataPin = 4;
+
+int leds = 0;
+
+void setup() 
+{
+  pinMode(latchPin, OUTPUT);
+  pinMode(dataPin, OUTPUT);  
+  pinMode(clockPin, OUTPUT);
+}
+
+void loop() 
+{
+  int deger  = analogRead(potPin);
+  int yanan_ledsayisi = deger / 114;  //1023 / 9
+  leds = 0;
+  for (int i = 0; i < yanan_ledsayisi; i++)
+  {
+    bitSet(leds, i); 
+  }
+  registeraYaz();
+}
+
+void registeraYaz()
+{
+   digitalWrite(latchPin, LOW);
+   shiftOut(dataPin, clockPin, LSBFIRST, leds);
+   digitalWrite(latchPin, HIGH);
+}
