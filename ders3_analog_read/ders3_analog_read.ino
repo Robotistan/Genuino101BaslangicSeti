dc488aee5f30624818ce07f8d4ba8532bf9a3e29
Yusuf Gündoğdu <yusuf@bomec.com.tr>
2017-07-11 14:42:10 +0300

    ders3_analog_read.ino hatalar düzeltildi
    setup() fonksiyonuna delay eklendi. loop() fonksiyonuna Serial.print("Analog pin degeri: "); eklendi.

--- ders3_analog_read/ders3_analog_read.ino
+++ ders3_analog_read/ders3_analog_read.ino
@@ -4,12 +4,12 @@ int deger = 0; //potansiyometreden okuyacagimiz deger icin bir degisken tanimliy
 void setup() 
 {
   Serial.begin(9600); //seri port iletisimini baslatiyor
-  Serial.println("Analog input okuma ornegi.");
+  delay(3000); //iletisimin baslayabilmesi icin 3sn bekleniyor
 }
 void loop() 
 {
+  Serial.print("Analog pin degeri: "); //on aciklama metnini yaziyor
   deger  = analogRead(potPin); //analog giristen okudugumuz degeri tanimladigimiz degiskene atiyor
   Serial.println(deger); //degeri seri port ekranina yazdiriyor
   delay(100); //her bir okuma arasinda 100ms bekleme suresi ekliyor
 }
-
